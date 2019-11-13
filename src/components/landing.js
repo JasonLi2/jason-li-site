9aea2564f5abd26f088b5525cdcce2cf4df1852f
JasonLi2 <thatjasonli@gmail.com>
2019-11-13 10:45:02 -0700

    Update landing.js

--- src/components/landing.js
+++ src/components/landing.js
@@ -13,7 +13,7 @@ const Container = styled(BackgroundImage)`
 const Content = styled.div`
   position: absolute;
   top: 40%;
-  left: 40%;
+  left: 48%;
   transform: translate(-50%, -50%);
   width: 75%;
   @media only screen and (max-width: 39em) {
